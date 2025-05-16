f59f74403b0c9ee45760a7175c8ca733a6d5d993
Adam Plaice <plaice.adam+github@gmail.com>
2025-05-16 19:59:06 +0200

    patch pipenv

---
+++ patch_pipenv.py
@@ -0,0 +1,4 @@
+with open("C:\hostedtoolcache\windows\Python\3.10.11\x64\lib\site-packages\pipenv\patched\pip\_internal/resolution/resolvelib/factory.py") as f:
+     factory_source = f.read()
+
+print(factory_source.split("\n")[679:681])
