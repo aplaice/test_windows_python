6aad65d46a79e05a0e6d14e9939b0513dc8a096e
Adam Plaice <plaice.adam+github@gmail.com>
2025-05-16 20:14:25 +0200

    try logger?

--- patch_pipenv.py
+++ patch_pipenv.py
@@ -3,7 +3,7 @@ with open("C:/hostedtoolcache/windows/Python/3.10.11/x64/lib/site-packages/pipen
 
 split_source = factory_source.split("\n")
 
-split_source.insert(681, '        print(cands)')
+split_source.insert(681, '        logger.critical(cands)')
 
 with open("C:/hostedtoolcache/windows/Python/3.10.11/x64/lib/site-packages/pipenv/patched/pip/_internal/resolution/resolvelib/factory.py", "w") as f:
     f.write("\n".join(split_source))
