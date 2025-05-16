156355d825eb3101196409216e57f07dfdf1eefa
Adam Plaice <plaice.adam+github@gmail.com>
2025-05-16 20:09:31 +0200

    actually patch now

--- patch_pipenv.py
+++ patch_pipenv.py
@@ -1,4 +1,9 @@
 with open("C:/hostedtoolcache/windows/Python/3.10.11/x64/lib/site-packages/pipenv/patched/pip/_internal/resolution/resolvelib/factory.py") as f:
      factory_source = f.read()
 
-print(factory_source.split("\n")[679:681])
+split_source = factory_source.split("\n")
+
+split_source.insert(681, 'print(cands)')
+
+with open("C:/hostedtoolcache/windows/Python/3.10.11/x64/lib/site-packages/pipenv/patched/pip/_internal/resolution/resolvelib/factory.py", "w") as f:
+    f.write("\n".join(split_source))
