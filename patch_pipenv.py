d677a68be5e8f74309edccfc623f56c25670e6f8
Adam Plaice <plaice.adam+github@gmail.com>
2025-05-16 21:10:43 +0200

    get more info 3

--- patch_pipenv.py
+++ patch_pipenv.py
@@ -34,7 +34,7 @@ patch_file(
 
 patch_file(
     PIPENV_ROOT + "patched/pip/_internal/index/package_finder.py",
-    767,
+    766,
     """
         logger.critical(result)
         logger.critical(detail)
